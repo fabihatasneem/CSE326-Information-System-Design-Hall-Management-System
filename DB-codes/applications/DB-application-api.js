9925f8cea59b49153e8d04a0bbfebf47924fc2bb
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-02-19 01:41:20 +0600

    Application queries updated

--- DB-codes/applications/DB-application-api.js
+++ DB-codes/applications/DB-application-api.js
@@ -1,40 +1,72 @@
 const Database = require('../database');
 const database = new Database();
 
-async function getAllStudents(){
-    const sql = `SELECT * FROM student`;
+async function getAllForwardedApplications(){
+    const sql = `SELECT * FROM application_for_seat WHERE status = 'forwaredToProvost'`;
     const binds = []
     const result = (await database.execute(sql, binds)).rows;
     return result;
 }
 
-async function getStudentsByDepartment(department){
-    const sql = `SELECT * FROM student
-                WHERE department = $1`;
-    const binds = [department]
+async function sortApplicationsBySeniority(){
+    const sql = `SELECT * FROM student s INNER JOIN application_for_seat a ON s.id = a.student_id
+                WHERE a.status = 'forwaredToProvost' ORDER BY s.degree DESC, s.level DESC`;
+    const binds = []
     const result = (await database.execute(sql, binds)).rows;
     return result;
 }
 
-async function getStudentsByBatch(level, term){
-    const sql = `SELECT * FROM student
-                WHERE degree = 'B.Sc' AND level = $2 AND term = $3`;
-    const binds = [level, term]
+async function sortApplicationsBySeniorityAndDistrict(dhakaFlag){
+    let sql;
+    if(dhakaFlag == 1) {
+        sql = `SELECT * FROM student s 
+                INNER JOIN application_for_seat a ON s.id = a.student_id
+                INNER JOIN address ad ON s.id = ad.id
+                WHERE a.status = 'forwaredToProvost' AND ad.district = $1
+                ORDER BY s.degree DESC, s.level DESC`;
+    }
+    else {
+        sql = `SELECT * FROM student s 
+        INNER JOIN application_for_seat a ON s.id = a.student_id
+        INNER JOIN address ad ON s.id = ad.id
+        WHERE a.status = 'forwaredToProvost' AND ad.district NOT IN ('Dhaka')
+        ORDER BY s.degree DESC, s.level DESC`;
+    }
+    const binds = [district]
     const result = (await database.execute(sql, binds)).rows;
     return result;
 }
 
-async function getStudentsByDegree(degree){
-    const sql = `SELECT * FROM student
-                WHERE degree = $1`;
-    const binds = [degree]
+async function sortApplicationsBySeniorityAndDistrictAndResult(dhakaFlag){
+    let sql;
+    if(dhakaFlag == 1) {
+        sql = `SELECT * FROM student s 
+                INNER JOIN application_for_seat a ON s.id = a.student_id
+                INNER JOIN address ad ON s.id = ad.id
+                WHERE a.status = 'forwaredToProvost' AND ad.district = $1
+                ORDER BY s.degree DESC, s.level DESC, s.cgpa DESC`;
+    } else {
+        sql = `SELECT * FROM student s 
+        INNER JOIN application_for_seat a ON s.id = a.student_id
+        INNER JOIN address ad ON s.id = ad.id
+        WHERE a.status = 'forwaredToProvost' AND ad.district NOT IN ('Dhaka')
+        ORDER BY s.degree DESC, s.level DESC, s.cgpa DESC`;
+    }
+    const binds = [district]
     const result = (await database.execute(sql, binds)).rows;
     return result;
+}
+
+async function rejectApplication(id){
+    const sql = `UPDATE application_for_seat SET status = 'rejected' WHERE id = $1`;
+    const binds = [id]
+    await database.execute(sql, binds);
 }
 
 module.exports = {
-    getAllStudents,
-    getStudentsByDepartment,
-    getStudentsByBatch,
-    getStudentsByDegree
+    getAllForwardedApplications,
+    sortApplicationsBySeniority,
+    sortApplicationsBySeniorityAndDistrict,
+    sortApplicationsBySeniorityAndDistrictAndResult,
+    rejectApplication
 }
