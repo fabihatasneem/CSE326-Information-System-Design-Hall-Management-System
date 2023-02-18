491bff79e1f0bddccfda3bb90a0fdea6f1726be2
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-02-19 00:26:42 +0600

    Notice queries updated

--- DB-codes/notices/DB-notice-api.js
+++ DB-codes/notices/DB-notice-api.js
@@ -2,14 +2,14 @@ const Database = require('../database');
 const database = new Database();
 
 async function getAllNotices(){
-    const sql = `SELECT notice.title, notice.notice_pdf, notice.upload_time, notice.staff_id, staff.name
+    const sql = `SELECT notice.id, notice.title, notice.notice_pdf, notice.upload_time, notice.staff_id, staff.name
                 FROM notice
                 INNER JOIN staff ON notice.staff_id = staff.id
                 WHERE notice.status = 'approved'
                 ORDER BY notice.upload_time DESC`;
     const binds = []
     const result = (await database.execute(sql, binds)).rows;
-    return result[0];
+    return result;
 }
 
 async function forwardNoticeToProvost(staff_id, notice_title, notice_pdf){
@@ -19,33 +19,30 @@ async function forwardNoticeToProvost(staff_id, notice_title, notice_pdf){
     const sql = `INSERT INTO notice (staff_id, upload_time, status, notice_pdf, title)
           VALUES ($1, $2, $3, $4, $5)`;
     const binds = [staff_id, upload_time, status, notice_pdf, notice_title]
-    const result = (await database.execute(sql, binds)).rows;
-    return result[0];
+    await database.execute(sql, binds);
 }
 
-async function showForwardedNotices(){
-    const sql = `SELECT notice.title, notice.notice_pdf, notice.upload_time, notice.staff_id, staff.name
+async function getForwardedNotices(){
+    const sql = `SELECT notice.id, notice.title, notice.notice_pdf, notice.upload_time, notice.staff_id, staff.name
                 FROM notice
                 INNER JOIN staff ON notice.staff_id = staff.id
                 WHERE notice.status = 'forwardedToProvost'
                 ORDER BY notice.upload_time DESC`;
     const binds = []
     const result = (await database.execute(sql, binds)).rows;
-    return result[0];
+    return result;
 }
 
 async function approveNotice(notice_id){
-    var status = "approved";
-    const sql = `UPDATE notice SET status = $1 WHERE id = $2`;
-    const binds = [status, notice_id]
+    const sql = `UPDATE notice SET status = 'approved' WHERE id = $2 RETURNING *`;
+    const binds = [notice_id]
     const result = (await database.execute(sql, binds)).rows;
     return result[0];
 }
 
 async function declineNotice(notice_id){
-    var status = "declined";
-    const sql = `UPDATE notice SET status = $1 WHERE id = $2`;
-    const binds = [status, notice_id]
+    const sql = `UPDATE notice SET status = 'declined' WHERE id = $2 RETURNING *`;
+    const binds = [notice_id]
     const result = (await database.execute(sql, binds)).rows;
     return result[0];
 }
@@ -53,7 +50,7 @@ async function declineNotice(notice_id){
 module.exports = {
     getAllNotices,
     forwardNoticeToProvost,
-    showForwardedNotices,
+    getForwardedNotices,
     approveNotice,
     declineNotice
 }
---
+++ DB-codes/users/DB-student-api.js
@@ -0,0 +1,102 @@
+const Database = require('../database');
+const database = new Database();
+
+async function getAllStudents(){
+    const sql = `SELECT * FROM student`;
+    const binds = []
+    const result = (await database.execute(sql, binds)).rows;
+    return result;
+}
+
+async function getStudentsByDepartment(department){
+    const sql = `SELECT * FROM student
+                WHERE department = $1`;
+    const binds = [department]
+    const result = (await database.execute(sql, binds)).rows;
+    return result;
+}
+
+async function getStudentsByBatch(level, term){
+    const sql = `SELECT * FROM student
+                WHERE degree = 'B.Sc' AND level = $2 AND term = $3`;
+    const binds = [level, term]
+    const result = (await database.execute(sql, binds)).rows;
+    return result;
+}
+
+async function getStudentsByDegree(degree){
+    const sql = `SELECT * FROM student
+                WHERE degree = $1`;
+    const binds = [degree]
+    const result = (await database.execute(sql, binds)).rows;
+    return result;
+}
+
+async function getStudentInfoById(id){
+    const sql = `SELECT * FROM student
+                INNER JOIN address ON student.id = address.id
+                WHERE student.id = $1 AND address.type = 'present'`;  //sending present address too for provost consideration in viva
+    const binds = [id]
+    const result = (await database.execute(sql, binds)).rows;
+    return result[0];
+}
+
+async function getPresentAddressById(id){
+    const sql = `SELECT * FROM address
+                WHERE id = $1 AND type = 'present'`;
+    const binds = [id]
+    const result = (await database.execute(sql, binds)).rows;
+    return result[0];
+}
+
+async function getPermanentAddressById(id){
+    const sql = `SELECT * FROM address
+                WHERE id = $1 AND type = 'permanent'`;
+    const binds = [id]
+    const result = (await database.execute(sql, binds)).rows;
+    return result[0];
+}
+
+async function getStudentAdditionalInfoById(id){
+    const sql = `SELECT * FROM student_additional_info
+                WHERE id = $1`;
+    const binds = [id]
+    const result = (await database.execute(sql, binds)).rows;
+    return result[0];
+}
+
+async function updateAdditionalInfoById(id){
+    const sql = `UPDATE student_additional_info
+                SET father_image = COALESCE($1,father_image),
+                father_phone = COALESCE($2, father_phone),
+                father_occupational_certificate = COALESCE($3, father_occupational_certificate),
+                mother_image = COALESCE($4, mother_image),
+                mother_phone = COALESCE($5, mother_phone),
+                mother_occupational_certificate = COALESCE($6, mother_occupational_certificate),
+                guardian_name = COALESCE($7, guardian_name),
+                guardian_image = COALESCE($8, guardian_image),
+                guardian_phone = COALESCE($9, guardian_phone),
+                student_id_card = COALESCE($10, student_id_card),
+                transcript = COALESCE($11, transcript),
+                birth_certificate = COALESCE($12, birth_certificate),
+                utility_bill = COALESCE($13, utility_bill),
+                siblings_document = COALESCE($14, siblings_document),
+                yearly_family_income = COALESCE($15, yearly_family_income),
+                parent_transfer_order = COALESCE($16, parent_transfer_order)
+                WHERE id = $17`;
+    const binds = [id]
+    const result = (await database.execute(sql, binds)).rows;
+    return result[0];
+}
+
+module.exports = {
+    getAllStudents,
+    getStudentsByDepartment,
+    getStudentsByBatch,
+    getStudentsByDegree,
+    getStudentInfoById,
+    getPresentAddressById,
+    getPermanentAddressById,
+    getStudentAdditionalInfoById,
+    updateAdditionalInfoById
+}
